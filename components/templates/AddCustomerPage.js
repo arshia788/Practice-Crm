11e6214a7c6a8238adba4eff9f297dc59cf611e5
Amirali Amanipour <arshia.ala80@gmail.com>
2023-07-26 15:54:00 +0330

    working on addcustomer component

--- components/templates/AddCustomerPage.js
+++ components/templates/AddCustomerPage.js
@@ -1,17 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function AddCustomerPage() {
+
+  const [form, setForm] = useState({
+    name: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    address: '',
+    postalCode: '',
+    date: '',
+  })
   return (
     <div >
-        <h2 className='text-white font-semibold text-lg'>AddCustomerPage</h2>
+      <h2 className='text-white font-semibold text-lg'>AddCustomerPage</h2>
 
-        <div className='flex justify-between items-center'>
-            <button 
-            className='bg-red-500 rounded'
-            >cancel</button>
-        </div>
+      <div className='flex justify-between items-center mt-4'>
+        <button
+          className='bg-red-500 rounded text-white px-2 py-1'
+        >cancel</button>
+
+        <button
+          className='bg-green-500 rounded text-white px-2 py-1'
+        >save</button>
+
+
+      </div>
     </div>
   )
 }
 
- 
