7b7cbd82cedc4bb30de0319fef5b5c4832955e20
Amirali Amanipour <arshia.ala80@gmail.com>
2023-07-26 20:20:27 +0330

    index pages

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,28 @@
+import connectDB from '../utils/connectDB';
+import Customer1 from '../models/Customer1';
 
-export default function index() {
+export default function index({data}) {
+  console.log(data);
+  
   return (
     <div >index</div>
   )
 }
 
- 
+export async function getServerSideProps() {
+  try {
+    await connectDB()
+    const customer= await Customer1.find()
+    return{
+      props:{
+        data:JSON.parse(JSON.stringify(customer))
+      }
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ status: "success", message: "failed to connect to DB" })
+    return;
+  }
+
+
+} 
