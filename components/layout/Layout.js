8c536aeb8c4a29642de77d8fe3adcef71db05478
Amirali Amanipour <arshia.ala80@gmail.com>
2023-07-26 13:17:42 +0330

    initial folder

---
+++ components/layout/Layout.js
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function Layout({ children }) {
+    return (
+        <div className='bg-black h-screen box-border p-4'>
+
+            <header className='w-10/12 mx-auto flex justify-between items-center text-white'>
+                <h2 className='text-zinc-50 text-xl font-semibold'>Next-Crm</h2>
+                <Link href='/add-customer' className=' rounded bg-green-500 px-4 py-1'>Add-Customer</Link>
+            </header>
+
+            <div className='min-h-10/12 w-10/12 mx-auto my-2'>
+                {children}
+            </div>
+
+            <footer className='w-10/12 mx-auto bg-zinc-300  text-center p-2 rounded'>
+                <p>CRM-PROJECT</p>
+            </footer>
+        </div>
+    )
+}
+
---
+++ components/templates/AddCustomerPage.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default  function AddCustomerPage() {
+  return (
+    <div>AddCustomerPage</div>
+  )
+}
+
